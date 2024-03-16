8b26cb1d349cb9c502778ce213319b569851777b
vaibhav494 <vaibhavgaming190@gmail.com>
2024-03-16 19:05:32 +0530

    added each subject portion

--- trimming_pdf.py
+++ trimming_pdf.py
@@ -1,13 +1,13 @@
 from PyPDF2 import PdfWriter, PdfReader
 # page numbering starts from 0
-start=51  #start
-end=71 #end SHOULD BE ACTUAL PAGE NUMBER
-infile = PdfReader('SEM 4 ANSWERS.pdf', 'rb')
+start = 35  #start
+end = 38 #end SHOULD BE ACTUAL PAGE NUMBER
+infile = PdfReader('portion.pdf', 'rb')
 output = PdfWriter()
 
 for i in range(start, end):
     p = infile.pages[i] 
     output.add_page(p)
 
-with open('Android_Dev_22-23_answer.pdf', 'wb') as f:
+with open('Android_portion.pdf', 'wb') as f:
     output.write(f)
