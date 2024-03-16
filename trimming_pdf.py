41a4b0ee3e39324d9259e6b8929b66101f360096
vaibhav494 <vaibhavgaming190@gmail.com>
2024-03-16 20:22:14 +0530

    Time table added

--- trimming_pdf.py
+++ trimming_pdf.py
@@ -1,13 +1,13 @@
 from PyPDF2 import PdfWriter, PdfReader
 # page numbering starts from 0
-start = 35  #start
-end = 38 #end SHOULD BE ACTUAL PAGE NUMBER
-infile = PdfReader('portion.pdf', 'rb')
+start = 2  #start
+end = 3 #end SHOULD BE ACTUAL PAGE NUMBER
+infile = PdfReader('fff.pdf', strict=False)
 output = PdfWriter()
 
 for i in range(start, end):
     p = infile.pages[i] 
     output.add_page(p)
 
-with open('Android_portion.pdf', 'wb') as f:
+with open('Theory_Time_Table.pdf', 'wb') as f:
     output.write(f)
