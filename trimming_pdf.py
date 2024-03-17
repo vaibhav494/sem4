8d2a1e8f6d1465c5f3fa04490b79dd9192e4097b
vaibhav494 <vaibhavgaming190@gmail.com>
2024-03-17 11:01:04 +0530

    added Dot net Unit wise Textbook

--- trimming_pdf.py
+++ trimming_pdf.py
@@ -1,13 +1,13 @@
 from PyPDF2 import PdfWriter, PdfReader
 # page numbering starts from 0
-start = 2  #start
-end = 3 #end SHOULD BE ACTUAL PAGE NUMBER
-infile = PdfReader('fff.pdf', strict=False)
+start = 576  #start
+end = 609 #end SHOULD BE ACTUAL PAGE NUMBER
+infile = PdfReader('Textbook.pdf', strict=False)
 output = PdfWriter()
 
 for i in range(start, end):
     p = infile.pages[i] 
     output.add_page(p)
 
-with open('Theory_Time_Table.pdf', 'wb') as f:
+with open('chapter 18.pdf', 'wb') as f:
     output.write(f)
